114d4309167cfc79cbdc691cf5f78fa5566d6e32
Triptidhawann <triptidhawan15@gmail.com>
2026-09-04 21:53:40 +0530

    Fix Netlify function routing

--- server/api.js
+++ server/api.js
@@ -1,5 +1,7 @@
 const serverless = require('serverless-http');
 const app = require('./src/app');
 
-// Wrap the Express app for Netlify Functions
-module.exports.handler = serverless(app);
+// Wrap the Express app for Netlify Functions, ensuring the router understands the Netlify path
+module.exports.handler = serverless(app, {
+  basePath: '/.netlify/functions'
+});
